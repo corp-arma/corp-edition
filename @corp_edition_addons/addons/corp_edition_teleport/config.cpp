5515a508fbcb131cc6e1d7af81acede9f9cf7029
zgmrvn <zgmrvn+github@gmail.com>
2017-03-30 12:17:21 +0200

    ajout du module téléport

---
+++ @corp_edition_addons/addons/corp_edition_teleport/config.cpp
@@ -0,0 +1,45 @@
+class CfgPatches {
+	class CORP_Edition_Teleport {
+		units[] = {"CORP_Module_Teleport"};
+		author = "CORP Modding Studio";
+		requiredVersion = 1.66;
+		requiredAddons[] = {"A3_Modules_F", "corp_edition_core"};
+	};
+};
+
+class CfgFunctions {
+	class CORP {
+		tag = "CORP";
+
+		class CORPEditionTeleport {
+			file = "\corp_edition_teleport\functions";
+			class Teleport_Init {};
+			class Teleport_UiStart {};
+		};
+	};
+};
+
+class CfgVehicles {
+	class Logic;
+	class Module_F: Logic {
+		class ModuleDescription {
+			class AnyStaticObject;
+		};
+	};
+
+	class CORP_Module_Teleport: Module_F {
+		scope = 2;
+		displayName = "Téléportation vers un cooéquiper";
+		icon = "\corp_edition_teleport\icon.paa";
+		category = "CORP_Modules";
+
+		function = "CORP_fnc_teleport_init";
+		functionPriority = 1;
+		isGlobal = 1;
+		isTriggerActivated = 0;
+		isDisposable = 0;
+		is3DEN = 0;
+	};
+};
+
+#include "ui\ui.hpp"
---
+++ @corp_edition_addons/addons/corp_edition_teleport/ui/ui.hpp
@@ -0,0 +1,54 @@
+#include "ctrl.hpp"
+
+class BaseTitle;
+class BaseBackgroundBox;
+class BaseListBox;
+class CancelButton;
+class ValideButton;
+
+class CORP_TeleportDialog {
+	idd = TELEPORT_DIALOG_IDD;
+
+	onLoad = "[] spawn CORP_fnc_teleport_uiStart;";
+
+	class controlsBackground {
+		class Title: BaseTitle {
+			text = "Téléportation";
+		};
+
+		class Background: BaseBackgroundBox {};
+	};
+
+	class Controls {
+		class PlayersList: BaseListBox {
+			idc = TELEPORT_LIST_IDC;
+
+			x = safeZoneX + safeZoneW * 0.4;
+			y = safeZoneY + safeZoneH * 0.21;
+			w = safeZoneW * 0.2;
+			h = safeZoneH * 0.52;
+
+			colorBackground[] = {0.1, 0.1, 0.1, 1};
+		};
+
+		class CloseButton: CancelButton {
+			idc = TELEPORT_CLOSE_IDC;
+
+			x = safeZoneX + safeZoneW * 0.4;
+			y = safeZoneY + safeZoneH * 0.74;
+			w = safeZoneW * 0.0975;
+			h = safeZoneH * 0.05;
+		};
+
+		class TeleportButton: ValideButton {
+			idc = TELEPORT_TELEPORT_IDC;
+
+			x = safeZoneX + safeZoneW * 0.5025;
+			y = safeZoneY + safeZoneH * 0.74;
+			w = safeZoneW * 0.095;
+			h = safeZoneH * 0.05;
+
+			text = "Téléportation";
+		};
+	};
+};
